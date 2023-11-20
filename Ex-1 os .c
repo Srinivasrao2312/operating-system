6dcc4df6f9ba4cd70202d1eff23b61f930671508
Srinivasrao2312 <112844809+Srinivasrao2312@users.noreply.github.com>
2023-11-20 08:56:52 +0530

    Add files via upload

---
+++ Ex-1 os .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = getpid(); 
+    pid_t ppid = getppid(); 
+
+    printf("Current Process ID: %d\n", pid);
+    printf("Parent Process ID: %d\n", ppid);
+
+    return 0;
+}
